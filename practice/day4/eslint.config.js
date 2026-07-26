39f6796e877089096fc09942c019d04a2f91b9a2
Kwang Myung Yu <sguys99@gmail.com>
2026-07-26 14:45:52 +0900

    :sparkles: Init files for day4
    Signed-off-by: Kwang Myung Yu <sguys99@gmail.com>

---
+++ practice/day4/eslint.config.js
@@ -0,0 +1,25 @@
+import prettierConfig from "eslint-config-prettier";
+// ... 기존 설정들 ...
+// 배열의 맨 끝에 prettierConfig 추가
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+])
---
+++ practice/day4/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="p-8 text-3xl font-bold text-blue-600">
+      Tailwind 연결 성공 ✅
+    </h1>
+  );
+}
+
+export default App;
